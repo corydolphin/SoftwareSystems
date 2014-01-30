68cd0deef37f52e2fa9490f094001a961ab8c4e4
wcdolphin <wcdolphin@gmail.com>
2014-01-30 00:12:39 -0500

    Refactored cards into three functions, added comments and header

--- cards.c
+++ cards.c
@@ -1,12 +1,21 @@
+/*
+    A simple card counting cli interface. Do not use this to count cards,
+    you will lose.
+
+    Written by Cory Dolphin for Allen Downey's Software Systems at Olin College, 2014
+    No rights reserved.
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 
 
 
-/* Prompts the user for input and puts the reply in the given buffer.
+/* Prompts the user for input, storing the reply in the card_name buffer.
 
-   User input is truncated to the first two characters.
+   User input is at most two characters.
 
    prompt: string prompt to display
    card_name: buffer where result is stored
@@ -17,6 +26,11 @@ void get_card_name(char *prompt, char *card_name){
 }
 
 
+/* Adjusts the current score according to a newly seen card's value
+
+    count: a pointer to the int where the current count is stored.
+    val:   the value of the newly seen card.
+*/
 void adjust_score(int *count, int val){
     if ((val > 2) && (val < 7)) {
         ++*count;
@@ -25,38 +39,49 @@ void adjust_score(int *count, int val){
     }
 }
 
+/* Converts a card name to a card value.
+
+    If card_name is 'X', or an unrecognized value, the value -1 is returned.
+
+    card_name: the buffer where the card's name is stored.
+*/
+int get_card_value(char *card_name){
+    int temp_val; //A temporary value used only in the default case
+
+    switch(card_name[0]) {
+        case 'K':
+        case 'Q': //Face cards King, Queen, Jack all receive the same value
+        case 'J':
+            return 10;
+        case 'A':
+            return 11;
+        case 'X':
+            return -1;
+        default:
+            temp_val = atoi(card_name);
+            if((temp_val < 1) || (temp_val > 10)){ //The card was unrecognized. Print and re-prompt
+                puts("I don't understand that value!");
+                return -1;
+            }
+            return temp_val;
+        }
+}
 
 int main()
 {
     char card_name[3];
     int count = 0;
+    int card_value;
     char prompt[] = "Enter a cardname!";
 
     while ( card_name[0] != 'X'){
         get_card_name(prompt, card_name);
+        card_value = get_card_value(card_name);
 
-        int val = 0;
-        switch(card_name[0]) {
-        case 'K':
-        case 'Q':
-        case 'J':
-            val = 10;
-            break;
-        case 'A':
-            val = 11;
-            break;
-        case 'X':
+        if(card_value <= -1){ //The card was either unrecognized, or EXIT was requested.
             continue;
-        default:
-            val = atoi(card_name);
-            if((val < 1) || (val > 10)){
-                puts("I don't understand that value!");
-                continue;
-            }
         }
-
-        adjust_score(&count, val);
-
+        adjust_score(&count, card_value);
         printf("Current count: %i\n", count);
     }
     return 0;
